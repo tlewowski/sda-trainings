5c58da69a71850c19504b0c741a22d7147269147
Tomasz Lewowski <tomasz.lewowski@ratelware.com>
2021-09-11 10:13:30 +0200

    Support for arguments

--- make-git.sh
+++ make-git.sh
@@ -2,8 +2,13 @@
 
 GIT_REPO=hello
 
+REPO_DIR=$1
+
 SCRIPT_DIR=$(dirname $0)
-export GIT_REPO=${GIT_REPO}
+export GIT_REPO=${REPO_DIR}/${GIT_REPO}
+
+# Take an argument that would define directory in which
+# the repository should be created
 
 ${SCRIPT_DIR}/init-git.sh
 
